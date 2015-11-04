8d5b47f108746557225fa2700668afd885460b73
Gil Colunga <gil.colunga@nordstrom.com>
2015-11-04 10:28:53 -0800

    first commit

---
+++ AdaWeb/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AdaWeb.Models;
+
+namespace AdaWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Another Title";
+            return View();
+        }
+
+        public ActionResult MovieDetail()
+        {
+            var model = new Movie
+            {
+                Name = "The Martian",
+                Rating = 4,
+                ReleaseDate = new DateTime(2015, 10, 1)
+            };
+            return View(model);
+        }
+
+
+        public ActionResult MovieList()
+        {
+            var model = new List<Movie>();
+            model.Add(new Movie
+            {
+                Name = "The Martian",
+                Rating = 4,
+                ReleaseDate = new DateTime(2015, 10, 1)
+            });
+            model.Add(new Movie
+            {
+                Name = "MadMax",
+                Rating = 3,
+                ReleaseDate = new DateTime(2015, 9, 1)
+            });
+
+            model.Add(new Movie
+            {
+                Name = "Gravity",
+                Rating = 4,
+                ReleaseDate = new DateTime(2013, 10, 1)
+            });
+
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ AdaWeb/Models/Movie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaWeb.Models
+{
+    public class Movie
+    {
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Rating { get; set; }
+
+
+        //private string _name;
+        //private DateTime _releaseDate;
+        //private int _rating;
+
+        //public string Name
+        //{
+        //    get { return _name; }
+        //    set { _name = value; }
+        //}
+    }
+}
